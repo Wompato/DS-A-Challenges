933ef528a54d581e45483762cbd06db0153eac45
Wompato <alexshelton1999@gmail.com>
2024-03-03 16:16:43 -0500

    completed hello-world and get-sum from 01-basic-challenges

---
+++ 01-basic-challenges-1/01-hello-world/hello-world.js
@@ -0,0 +1,9 @@
+/**
+ * Returns a string containing 'Hello World!'.
+ * @returns {string} - The string 'Hello World!'.
+ */
+function helloWorld() {
+    return 'Hello World!';
+}
+
+module.exports = helloWorld;
